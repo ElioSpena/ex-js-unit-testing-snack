584af2e0b9934c1cd091d576a32446addd5204a1
ElioSpena <elio.spena.94@gmail.com>
2026-05-12 19:17:12 +0200

    feat: snack completi

--- index.js
+++ index.js
@@ -5,4 +5,34 @@ function getInitials(user) {
     .join("");
 }
 
-module.exports = { getInitials };
+function createSlug(string) {
+  if (string === "" || !isNaN(string)) {
+    throw new Error("Inserire un titolo valido!");
+  }
+  return string.toLowerCase().replaceAll(" ", "-");
+}
+
+function average(arr) {
+  return arr.reduce((acc, num) => acc + num, 0) / arr.length;
+}
+
+function isPalindrome(string) {
+  return (
+    string.toLowerCase() === string.split("").reverse().join("").toLowerCase()
+  );
+}
+
+function findPostById(arr, id) {
+  if (id > arr.length || id < 1) {
+    throw new Error("Inserire un numero valido!");
+  }
+  return arr.find((p) => p.id === id);
+}
+
+module.exports = {
+  getInitials,
+  createSlug,
+  average,
+  isPalindrome,
+  findPostById,
+};
